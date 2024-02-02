acc06164a6d68a62accb35f45fe37816487176ea
Lia-O1 <133197997+Lia-O1@users.noreply.github.com>
2024-02-02 18:09:54 +1100

    POST endpoint for creating new prompts in the db

---
+++ app/api/prompt/new/route.js
@@ -0,0 +1,18 @@
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const POST = async (req) => {
+  const { prompt, tag, userId } = await req.json();
+  try {
+    await connectToDB();
+    const newPrompt = new Prompt({
+      creator: userId,
+      prompt,
+      tag,
+    });
+    await newPrompt.save();
+    return new Response(JSON.stringify(newPrompt), { status: 201 }); //201 Created
+  } catch (error) {
+    return new Response("Failed to create a new prompt", { status: 500 }); //500 Server Error
+  }
+};
--- components/Form.jsx
+++ components/Form.jsx
@@ -31,7 +31,7 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
           </span>
           <input
             value={post.tag}
-            onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+            onChange={(e) => setPost({ ...post, tag: e.target.value })}
             placeholder="#tag"
             required
             className="form_input"
